3968042481c7dc6275e11d25d21bc5352eeeaeb4
cmachsocket <cmach_socket@outlook.com>
2026-07-14 03:21:17 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.lsposed.fuckthevideo"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "0.3"
+        versionCode = 6
+        versionName = "0.3.1"
     }
 
     buildTypes {
